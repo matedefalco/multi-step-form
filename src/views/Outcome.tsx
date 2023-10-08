177f6be37011463abd0187e2f9f501f019eaf936
Suka <mateodefalco1@gmail.com>
2023-10-08 12:37:23 -0300

    Outcome update

--- src/views/Outcome.tsx
+++ src/views/Outcome.tsx
@@ -1,5 +1,30 @@
-const Outcome = () => {
-	return <div>Outcome</div>
+import React from "react"
+import { useUserAnswers } from "@/context/GameContext"
+import { Card } from "@/components/ui/card"
+import { Button } from "@/components/ui/button"
+import { PageProps } from "@/types/IPages"
+
+const Outcome: React.FC<PageProps> = ({ onClick }) => {
+	const { userAnswers, updateUserAnswers } = useUserAnswers()
+	const handlePlayAgain = () => {
+		updateUserAnswers("", "")
+		onClick()
+	}
+
+	return (
+		<div className="flex flex-col items-center gap-8">
+			<Card className="p-8">
+				<ul>
+					{Object.entries(userAnswers).map(([questionId, outcome]) => (
+						<li key={questionId} className="text-center m-auto italic">
+							{outcome}
+						</li>
+					))}
+				</ul>
+			</Card>
+			<Button onClick={handlePlayAgain}>Play again</Button>
+		</div>
+	)
 }
 
 export default Outcome
